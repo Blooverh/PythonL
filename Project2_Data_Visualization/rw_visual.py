8523ab15d4af7376dbd3fbc7f4b3c30be98e3796
Diogo Aires <63264160+Blooverh@users.noreply.github.com>
2023-03-09 15:29:09 -0600

    update

--- Project2_Data_Visualization/rw_visual.py
+++ Project2_Data_Visualization/rw_visual.py
@@ -8,10 +8,10 @@ while True:
 
     #plot the points in the walk
     plt.style.use('classic')
-    fig, ax= plt.subplots()
+    #fill the screen with plot at a fixed size (15,9)
+    fig, ax= plt.subplots(figsize=(15,9))
     point_numbers= range(rw.num_points) # point numbers are all num_points points that will be used for coloring on next line 
     ax.scatter(rw.x_val, rw.y_val, c=point_numbers, cmap=plt.cm.Reds, edgecolors='none', s=1) # s is changed from 15 to 1 since we have 50000 points which is a lot 
-
     #Emphasize the first and last points 
     ax.scatter(0,0, c='green', edgecolors='none',s=100)
     ax.scatter(rw.x_val[-1], rw.y_val[-1], c='red', edgecolors='none', s=100)
